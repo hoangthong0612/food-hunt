eca0bba84ee708417848d7b5700a84a224e6382d
hoangthong0612 <hoangthongedm@gmail.com>
2025-03-14 15:00:07 +0700

    update

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ prev.addEventListener("click", () => {
   setTimeout(() => {
     items[0].classList.remove("prev-animation");
   }, 500);
-  //   items[0].transform = 'rotate(234deg) translate(150px) rotate(-234deg) translate(-50%, -50%)'
 });
 
 next.addEventListener("click", () => {
@@ -27,13 +26,13 @@ function updateMainImage(type) {
   let items = document.querySelectorAll(".item");
   let imageDisplay = document.querySelector(".main_images img:nth-child(1)");
   let imageHidden = document.querySelector(".main_images img:nth-child(2)");
+  
   imageDisplay.src = items[0].src;
+  document.documentElement.style.setProperty('--primary-color', items[0].dataset.color);
   if (type === "prev") {
     imageHidden.src = items[items.length - 1].src;
-    // imageHidden.style.opacity = 1;
   } else if (type === "next") {
     imageHidden.src = items[1].src;
-    // imageHidden.style.opacity = 1;
   }
 
   imageDisplay.classList.add("display-image-animation");
@@ -42,7 +41,6 @@ function updateMainImage(type) {
     imageDisplay.classList.remove("display-image-animation");
     imageHidden.classList.remove("hidden-image-animation");
   }, 500);
-  
 }
 
 updateMainImage();
